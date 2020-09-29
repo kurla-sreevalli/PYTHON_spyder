fe7a3a8a3f116d39ebc4225a8bfe7e5766f43f0a
Kurla Sreevalli <kurla.sreevalli@gmail.com>
2020-09-29 14:33:58 +0530

    Add files via upload
    Basics like the type of objects, use of comments, print function, and a string demo.

---
+++ 1st_session.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 18:34:31 2020
+
+@author: sreevalli kurla ksv
+"""
+#Python have OBJECTS-6#
+#number(int or any decimal/float)
+val=10
+aval=10.25
+#string
+name='python'
+#list
+alist=[10,20,30]
+#tuple
+atup=(10,20,30)
+#dictionary
+adic={10,20,30}
+#set
+aset=(10,20,30)
+
+
+
+#print function
+print("PYTHON PROGRAMMING")
+
+print(10,20,30)
+print("the values are:", 10,20,30)
+
+val=10
+aval=20
+print("the values are:", val,aval)
+
+
+
+#string demo
+a_name='python'
+b_name="spyder"
+c_name="""jupiter"""
+
+print(a_name)
+print(b_name)
+print(c_name)
+print("i love", a_name, b_name)
+
